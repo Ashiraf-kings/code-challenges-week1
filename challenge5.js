47a000098fc637c2a94a8d8ebb54a213d0515a99
“Ashiraf-kings” <ashirafkings12@gmail.com>
2020-10-29 10:19:31 -0700

    changing the grading system to a fuction

--- challenge5.js
+++ challenge5.js
@@ -1,17 +1,19 @@
 /*This is a grading system that tells the user his/her grade according to the marks inserted*/
-var grade = 10 ;
-
-if (grade >=85) {
+function grade(marks){
+if (marks >=85) {
 console.log("D1")
-}else if (grade>=75) {
+}else if (marks>=75) {
     console.log("D2")
-}else if (grade>=65) {
+}else if (marks>=65) {
     console.log("D3")
-}else if (grade>=40) {
+}else if (marks>=40) {
     console.log("D4")
-}else if  (grade>=20) {
+}else if  (marks>=20) {
     console.log("U")
-}else if (grade>=0) {
+}else if (marks>=0) {
     console.log("you are advised to search for another school")
-};
+}
+}
+grade(70)
+
     
